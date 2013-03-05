ef547c8b64f46540bc71f23da04b6b2ffed72a26
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-03-05 17:01:15 -0500

    data load script fixed

--- conf/data/cato_data.sql
+++ conf/data/cato_data.sql
@@ -4,7 +4,7 @@ INSERT INTO `user_password_history` (`user_id`, `password`, `change_time`)
 	VALUES ('0002bdaf-bfd5-4b9d-82d1-fd39c2947d19','#ADMINPASSWORD#',now());
 INSERT INTO `login_security_settings` (`id`, `pass_max_age`, `pass_max_attempts`, `pass_max_length`, `pass_min_length`, `pass_complexity`, `pass_age_warn_days`, `pass_require_initial_change`, `auto_lock_reset`, `login_message`, `auth_error_message`, `pass_history`, `page_view_logging`, `report_view_logging`, `allow_login`, `new_user_email_message`, `log_days`) 
 	VALUES (1,90,99,15,6,0,5,0,5,'New Cato Install','Login Error - Please check your user id and password.',0,0,0,1,'',90);
-INSERT INTO `messenger_settings` (`id`, `mode_off_on`, `loop_delay_sec`, `retry_delay_min`, `retry_max_attempts`, `smtp_server_addr`, `smtp_server_user`, `smtp_server_password`, `smtp_server_port`, `smtp_timeout`, `smtp_sl`, `from_email`, `from_name`, `admin_email`) 
+INSERT INTO `messenger_settings` (`id`, `mode_off_on`, `loop_delay_sec`, `retry_delay_min`, `retry_max_attempts`, `smtp_server_addr`, `smtp_server_user`, `smtp_server_password`, `smtp_server_port`, `smtp_timeout`, `smtp_ssl`, `from_email`, `from_name`, `admin_email`) 
 	VALUES (1,'off',30,1,5,'','','',25,10,0,'user@example.com','Cloud Sidekick Messenger','');
 INSERT INTO `poller_settings` (`id`, `mode_off_on`, `loop_delay_sec`, `max_processes`, `app_instance`) 
 	VALUES (1,'on',3,4,'DEFAULT');
