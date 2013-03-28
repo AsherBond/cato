12ddd48ff3d6799aa44a0dea80c142c158df0e85
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-03-28 15:02:34 -0400

    removed old utility scripts.

--- debug_windows.sh
+++
@@ -1,2 +0,0 @@
-osascript -e 'tell application "Terminal" to do script "cd $CATO_HOME/logfiles;tail -f cato_admin_ui.log;"'
-osascript -e 'tell application "Terminal" to do script "cd $CATO_HOME/logfiles;tail -f cato_rest_api.log;"'
--- taillogs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "### REST API ###"
-tail -10 $CATO_HOME/logfiles/cato_rest_api.log
-
-
-echo "### ADMIN UI ###"
-tail -10 $CATO_HOME/logfiles/cato_admin_ui.log
