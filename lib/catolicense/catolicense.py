77c7408886212b0ff2697df9b10ad4b6efd77040
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-04-12 16:45:40 -0400

    License check no longer crashes on non-ascii license files, instead shows default message.

--- lib/catolicense/catolicense.py
+++ lib/catolicense/catolicense.py
@@ -1,32 +1,31 @@
 def check_license(app=""):
     # this is important... if anything fails on the license check...
     # we ALLOW access.  Don't want any bugs to limit the ability to use the software.
+    filename = "../../LICENSE"
+    with open(filename, 'r') as f_in:
+        license_text = f_in.read()
+
+    # check the encoding to see if it's ascii, if not, use the default message
     try:
-        filename = "../../LICENSE"
-        with open(filename, 'r') as f_in:
-            license_text = f_in.read()
+        license_text.encode("ascii")
+    except:
+        license_text = None
 
-        if not license_text:
-            license_text = """<p>
-                    Copyright 2012 Cloud Sidekick
-                </p>
-                <p>
-                    Use of this software indicates agreement with the included software LICENSE.
-                </p>
-                <p>
-                    The LICENSE can be found in the application directory where this Cloud Sidekick product is installed.
-                </p>"""
-            
-        # the value will either be 'agreed' or ''
-        from catosettings import settings
-        license_status = settings.settings.get_application_setting("general/%slicense_status" % ("%s_" % app if app else ""))
+    if not license_text:
+        license_text = """Copyright 2012 Cloud Sidekick
+
+                Use of this software indicates agreement with the included software LICENSE.
+
+                The LICENSE can be found in the application directory where this Cloud Sidekick product is installed.
+                """
         
-        # the value will either be 'agreed' or ''
-        if license_status == "agreed":
-            return "pass", "", ""
-        else:
-            # not agreed, return the LICENSE file.
-            return "fail", "", license_text
-
-    except Exception as ex:
-        raise ex
+    # the value will either be 'agreed' or ''
+    from catosettings import settings
+    license_status = settings.settings.get_application_setting("general/%slicense_status" % ("%s_" % app if app else ""))
+    
+    # the value will either be 'agreed' or ''
+    if license_status == "agreed":
+        return "pass", "", ""
+    else:
+        # not agreed, return the LICENSE file.
+        return "fail", "", license_text
