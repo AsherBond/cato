9dfa0589a15b6a086a0c9e2c1b15754803da8d6a
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-05-07 15:07:25 -0400

    tag script now creates 'latest' tag.

--- tag_release_and_bump.sh
+++ tag_release_and_bump.sh
@@ -6,13 +6,20 @@ if [ "$version" = "" ] ; then
     echo "usage: $0 <release>"
     exit 1
 fi
+
+# 'latest' version
+git tag -d latest
+git push origin :refs/tags/latest
+
+# supplied version
 git tag -d $version
 git push origin :refs/tags/$version
 git pull
 echo $version > VERSION
 
 git add VERSION
-git commit -m "bump release to $release"
+git commit -m "Bumped Version to [$release]."
 git push
+git tag -a latest -m "Version $version"
 git tag -a $version -m "Version $version"
 git push --tags
