7df8ee5db56c4e1e058f88fd50418524ab68effe
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-07-10 14:55:26 -0400

    new column for deployment runstate

--- updatedb.py
+++ updatedb.py
@@ -93,6 +93,7 @@ versions = [
                       ]
              ],
             ["1.18", [
+                      ["addcolumn", "deployment", "runstate", "varchar(16) NULL after `health`"],
                       ["addcolumn", "application_settings", "license", "TEXT NULL"],
                       ["changecolumn", "task_step_user_settings", "button", "button varchar(1024)"],
                       ["sql", "delete from task_step_user_settings"]
