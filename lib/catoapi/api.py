ca9cfbf5219aa50df88ea4672405a009d2637299
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2013-10-23 17:41:15 -0400

    API - will now write informational messages to the log file as well as exceptions.

--- lib/catoapi/api.py
+++ lib/catoapi/api.py
@@ -355,9 +355,10 @@ class response:
 	def Write(self, output_format=""):
 		"""Returns the response in whatever format is specified here."""
 		
-		# IF there's an error code, set the HTTP status to 400
+		# IF there's an error code, set the HTTP status to 400, and write to the logfile
 		if self.ErrorCode:
 			web.ctx.status = "400 Bad Request"
+			logger.error("%s, %s, %s" % (self.ErrorCode, self.ErrorMessage, self.ErrorDetail))
 
 		if output_format == "text":
 			return self.asText()
