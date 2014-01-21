71241637363ca5de407cc5e4309944a1fbcbb2f3
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2014-01-21 14:49:28 -0500

    updated install.sh and requirements.txt to fix issues installing on CentOS

--- install.sh
+++ install.sh
@@ -185,7 +185,7 @@ else
 fi
 
 ### install third party python packages
-pip install -q -r $CATO_HOME/requirements.txt
+pip install -q -r $CATO_HOME/requirements.txt --allow-external jsonpath --allow-unverified jsonpath
 
 ### compile c++ encryption library
 cd $CATO_HOME/src/catocrypt
