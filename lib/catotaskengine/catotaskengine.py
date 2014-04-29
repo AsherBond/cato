db5e2bca3e218925dea7767ba7c5c60ab95db616
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2014-04-29 12:38:47 -0400

    update taskengine copyright notice

--- lib/catotaskengine/catotaskengine.py
+++ lib/catotaskengine/catotaskengine.py
@@ -1,5 +1,5 @@
 ########################################################################/
-# Copyright 2011 Cloud Sidekick
+# Copyright 2014 Cloud Sidekick
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
